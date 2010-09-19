f930ef02b8b7264a5375b5bf4c79aae9a326d5d5
Christopher Manning <c@christophermanning.org>
2010-09-19 08:49:12 +0000

    added build file

---
+++ build.ps1
@@ -0,0 +1,25 @@
+# Windows Powershell 2.0
+# To allow running powershell scripts: set-executionpolicy remotesigned
+
+$Directory = Get-Item .
+$BuildFileName = $Directory.FullName + "\chicagobustrackergadget.gadget"
+
+# Check if build already exists
+if (test-path $BuildFileName) {
+  echo "Build File " + $BuildFileName + " Already Exists"
+  return
+}
+
+# Create .tmp
+Copy-Item src .tmp -recurse
+
+# Remove svn folders from .tmp
+Get-ChildItem .tmp -recurse -force -include .svn | foreach -process {Remove-Item $_.FullName -force -recurse}
+
+# Zip .tmp Contents
+set-content $BuildFileName ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18)) 
+$ZipFile = (new-object -com shell.application).NameSpace($BuildFileName) 
+dir .tmp | foreach {$ZipFile.CopyHere($_.fullname);Start-sleep -milliseconds 500} 
+
+# Delete .tmp Resources
+Remove-Item .tmp -recurse -force
